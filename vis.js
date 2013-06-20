71e7964cf5e1eed930bf334ce49d416e366574d8
Geoff H <horne@slc.com.au>
2013-06-19 19:19:05 -0700

    code cleanup, mostly stable, sans json reloads

--- vis.js
+++ vis.js
@@ -421,6 +421,11 @@ var myGraph = {
 
         console.log ( "mygraph init" );
 
+// there is a problem here involving REFS, once you make a call to 
+// d3.layout.force() all sorts of REFS to the links and nodes list get
+// created and mesed with, so we have to create all these arrays
+// BEFORE we make a call to force.links()
+
         this.d3Layout = d3.layout.force()
             // point to the JSON dataset
             .nodes(this.nodes)
@@ -430,7 +435,7 @@ var myGraph = {
             .size([width, height])
             // the tick method creates the layout, so it needs to return the
             // nodes and link info, see below
-            .on("tick", tick);
+            .on("tick", this.tick);
 
         /*
         force = d3.sankey()
@@ -537,6 +542,37 @@ var myGraph = {
         this.getView().links = slinks ;
 
         this.d3Layout.start();
+
+    },
+
+    tick: function() {
+
+        // tick switches to it's own namespace, so we have to
+        // find a way to get to the veriables we require
+        // without reverting to a global namesspace
+
+        var v = myGraph.getView();
+
+        // since we now have a parent 'g' object on the circle
+        // we use the transform() to move things around
+        v.nodes
+            .attr("transform", function(d) { 
+                return "translate(" + d.x + "," + d.y + ")"; });
+
+        /*
+        svgNodes.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; })
+          */
+
+        /*
+        */
+
+        // [ ] uncomment this!!!
+        v.links
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
 
     },
 
@@ -654,253 +690,6 @@ setTimeout(function() {
 },4000);
 */
 
-// ---------------------------
-// set up input event handlers
-
-
-function getHostFlows( json ) {
-
-    // we have to assume the node list is undefined,
-    // but the host list is something we can work from
-
-    if ( json.hosts.length > MAXNODES ) {
-        // we need a summary by group
-        getGroupLinks( json );
-
-        // and we need a host lookup index
-        reindexHosts( json );
-
-        //console.log ( "hix" , json.hostIndex );
-
-        // so we can create a summary index of flows
-        var flowIndex = {};
-        json.flows.forEach(function(d, i) {
-            // find the source and dst type
-            // and summarise flows by type
-
-            var st,dt
-            if ( json.hostIndex[d.src] ) {
-                st= json.hostIndex[d.src].type
-            }
-            else {
-                console.log ( "Bad f src" , d );
-            }
-            if ( json.hostIndex[d.dst] ) {
-                dt= json.hostIndex[d.dst].type
-            }
-            else {
-                console.log ( "Bad f dst" , d );
-            }
-
-            //console.log ( "new flow : " , st , dt );
-
-            if ( st && dt ) {
-                if ( ! flowIndex[st] ) {
-                    flowIndex[st] = {}
-                }
-                flowIndex[st][dt]=1;
-            }
-
-        });
-
-        // console.log( 'fix' , flowIndex );
-
-        // now walk the flow summaries
-        Object.keys(flowIndex).forEach(function(k, i) {
-            var fs = k;
-            Object.keys(flowIndex[fs]).forEach(function(k, i) {
-                var fd = k;
-
-                //console.log ( "add flow : " , fs , fd );
-
-                if ( ! ( fs === fd ) ) {
-                    json.links.push({
-                        src: fs,
-                        dst: fd
-                    });
-                }
-            });
-        });
-
-    }
-    else {
-        setNodesToHosts( json );
-        json.links.push.apply( json.links , json.flows );
-    }
-
-    return 1;
-}
-
-function getSwitchInterconnects( json ) {
-
-    // [ ] ADD, we don't reset!!
-    // [ ] I should use an index to refs in the array...
-
-    // add additional links showing the interconnects
-    // [ ] assume the uplink array is preformatted
-    json.links.push.apply( json.links , json.uplinks );
-
-    return 1;
-}
-
-function getSwitchLinks( json ) {
-
-    // reset the nodes array
-    // [ ] I should use an index to refs in the array...
-
-    setNodesToHosts( json );
-
-    json.nodes.push.apply( json.nodes , json.switches );
-    //json.nodes.push.apply( json.nodes , json.hosts );
-
-    // and link hosts to switches
-    //json.links.length = 0 ;
-    if ( json.hosts.length < MAXNODES ) {
-        json.hosts.forEach(function(d, i) {
-            json.links.push({
-                src: d.id,
-                dst: d.ovs
-            });
-        });
-    }
-
-    //console.log ( 'gsl' , json );
-
-    return 1;
-}
-
-function getGroupLinks( json ) {
-    // list the host groups and link all VISIBLE hosts
-    // to the groups
-
-    // we can't change where the link array points, so we have to
-    // manipulate it from here
-
-    // reset the data
-    setNodesToHosts( json );
-
-    json.nodes.push.apply( json.nodes , getGroupNodes(json) );
-
-    // now walk each of the hosts array
-    // and create links to their parent node
-    if ( json.hosts.length < MAXNODES ) {
-        json.hosts.forEach(function(d, i) {
-            json.links.push({
-                src: d.id,
-                dst: d.type
-            });
-        });
-    }
-
-    return 1 ;
-}
-
-function getGroupNodes( json ) {
-    // return an array of nodes that are the unique groups
-    var gnodes = [] ;
-    // then add them as nodes
-    Object.keys(json.types).forEach(function(k, i) {
-        gnodes.push({
-            id: k,
-            name: k + " - " + json.types[k],
-            type: k,
-            nodetype: 'root'
-        });
-    });
-
-    return gnodes ;
-}
-
-function setNodesToHosts( json ) {
-    // this is usually the initial state
-    // reset the data
-    json.nodes.length = 0 ;
-    json.links.length = 0 ;
-
-    // get unique categories for each host type
-    json.types = {};
-    json.hosts.forEach(function(d, i) {
-        if ( ! json.types[d.type] ) {
-            json.types[d.type] = 1;
-        }
-        else {
-            json.types[d.type]++ ;
-        }
-    });
-
-    //console.log ( 'types' , json.types );
-
-    // if we have too many hosts, we need to summarise, and don't report
-    // hosts
-    //  json.nodes.push.apply( json.nodes , getGroupNodes(json) );
-    if ( json.hosts.length < MAXNODES ) {
-        json.nodes.push.apply( json.nodes , json.hosts );
-    }
-    else {
-        json.nodes.push({
-            "id": "summ", 
-            "name": json.hosts.length + " hosts",
-            "type": "summary", 
-             "ovs": ""
-        });
-    }
-
-    return 1;
-}
-
-function reindexHosts( json ) {
-
-    // index the nodes by 'id'
-    json.hostIndex = {};
-    json.hosts.forEach(function(d, i) {
-        json.hostIndex[d.id] = d;
-    });
-
-}
-
-function reindexNodes( json ) {
-
-    // index the nodes by 'id'
-    json.nodeIndex = {};
-    json.nodes.forEach(function(d, i) {
-        json.nodeIndex[d.id] = d;
-    });
-
-}
-
-function reindexLinks( json ) {
-
-    // links are ALWAYS refs to a node ID, in thia APP,
-
-    // but D3 wants a link to point to an ARRAY index (buggy)
-    // or a var/ref to an actual node object.
-
-    // as the node array is always changing, the safe thing to do
-    // is, after we've built the node array, wak the link array
-    // and compile in refs to the actual nodes
-
-    // WARNING: This will get unwieldy with large datasets
-    // we should find a way to pop and push links instead
-
-    reindexNodes( json );
-
-    // now walk the links array and insert refs to the nodes
-    // force.links uses 'd.source' and 'd.target'
-    json.links.forEach(function(d, i) {
-      d.source = json.nodeIndex[d.src];
-      d.target = json.nodeIndex[d.dst];
-    });
-
-}
-
-
-// WARNING:..
-
-// there is a problem here involving REFS, once you make a call to 
-// d3.layout.force() all sorts of REFS to the links and nodes list get
-// created and mesed with, so we have to create all these arrays
-// BEFORE we make a call to force.links()
-
 //-------------------
 
 function getClassSize(d) {
@@ -908,32 +697,3 @@ function getClassSize(d) {
     return d.nodetype === 'root' ? 16 : 8;
 }
 
-function tick() {
-    // tick switches to it's own namespace, so we have to
-    // find a way to get to the veriables we require
-    // without reverting to a global namesspace
-
-    var v = myGraph.getView();
-
-    // since we now have a parent 'g' object on the circle
-    // we use the transform() to move things around
-    v.nodes
-        .attr("transform", function(d) { 
-            return "translate(" + d.x + "," + d.y + ")"; });
-
-    /*
-    svgNodes.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-      */
-
-    /*
-    */
-
-    // [ ] uncomment this!!!
-    v.links
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-}
